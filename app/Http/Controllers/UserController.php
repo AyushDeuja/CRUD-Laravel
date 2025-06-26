2897086ae28eaa04047f0266a24e0fbd71668e9e
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-26 13:59:37 +0545

    database connection and unique validation done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function regiser (Request $request) {
         $incomingFields = $request -> validate([
             'name' => ['required', 'min:3', 'max: 15'],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:6', 'max: 200'],
 
         ]);
-        return 'Hello from Controller';
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth() -> login($user);
+        return redirect('/');
     }
 }
