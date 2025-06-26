68eaeff111e694ac4ea9000dde1e15b5609347f3
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-26 13:17:36 +0545

    validation done in form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function regiser () {
+    public function regiser (Request $request) {
+        $incomingFields = $request -> validate([
+            'name' => ['required', 'min:3', 'max: 15'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6', 'max: 200'],
+
+        ]);
         return 'Hello from Controller';
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
         <h2>Register</h2>
         <form action="/register" method="POST">
             @csrf
-            <input type="text" placeholder="name" style="padding: 2px">
-            <input type="text" placeholder="email">
-            <input type="password" placeholder="password">
+            <input type="text" name="name" placeholder="name" style="padding: 2px">
+            <input type="text" name="email" placeholder="email">
+            <input type="password" name="password" placeholder="password">
             <button>Register</button>
         </form>
     </div>
